797c4c6e9af3bc2c29e522bcb98b5ceebf9d5607
SoerenHoffmann <Soeren.hoffmann@gmx.net>
2022-07-28 15:06:15 +0200

    Implemented Random Color BG via JS

--- app.js
+++ app.js
@@ -9,9 +9,35 @@ function sendMail() {
 function addTodoInList(){
     let todoListInput = document.getElementById("myInput").value;
     let todoListUL = document.getElementById("myUL");
-    let emptyListItem = document.createElement("li");
-    let textNodeTodo = document.createTextNode(todoListInput);
-    emptyListItem.appendChild(textNodeTodo);
-    todoListUL.appendChild(emptyListItem);
-    document.getElementById("myInput").value = "";
+
+    if (todoListInput != ""){
+        let emptyListItem = document.createElement("li");
+        let textNodeTodo = document.createTextNode(todoListInput);
+        emptyListItem.appendChild(textNodeTodo);
+        todoListUL.appendChild(emptyListItem);
+        document.getElementById("myInput").value = "";
+    }else{
+        //alert("Line is empty.")
+    }
 }
+
+function randomColor(){
+    let rgb = "rgba(";
+    for (let i = 0; i < 3; i++){
+        rgb += Math.floor(Math.random()*255) + ",";
+
+    }
+
+    return rgb;
+}
+
+function settingNewValue(){
+    let randomValue0 = randomColor();
+    let randomValue1 = randomColor();
+    document.documentElement.style.setProperty("--randomColor0",randomValue0+"1)");
+    document.documentElement.style.setProperty("--randomColor1",randomValue1+"1)");
+
+}
+
+setInterval(settingNewValue,5000);
+
