9023ca5303bf9faaf8a20776bcd85ee225338698
OthmanAdi <github_me@outlook.de>
2022-07-28 15:06:18 +0200

    Implemented javascript BG loop

--- app.js
+++ app.js
@@ -5,15 +5,37 @@ function sendMail() {
         + "&body=" + encodeURIComponent(document.getElementById("emailInput").value);
 }
 
-
 function addTodoinList() {
     let todoListInput = document.getElementById("myInput").value;
     let textNodeTodo = document.createTextNode(todoListInput);
     let todoListUL = document.getElementById("myUL");
     let emptyListItem = document.createElement("li");
-
     emptyListItem.appendChild(textNodeTodo);
-    todoListUL.appendChild(emptyListItem);
+
     console.log("Todo has been added!");
     document.getElementById("myInput").value = "";
+
+    if (todoListInput === "") {
+        alert("Line is Empty, please enter Todo in the input field")
+    } else {
+        todoListUL.appendChild(emptyListItem);
+    }
 }
+
+function randomColor() {
+    let rgb = "rgba(";
+    for (let start = 0; start < 3; start++) {
+        rgb += Math.floor(Math.random() * 255) + ",";
+        console.log(rgb);
+    }
+    return rgb
+}
+
+function settingNewValue() {
+    let randomValue0 = randomColor();
+    let randomValue1 = randomColor();
+    document.documentElement.style.setProperty("--randomColor0", randomValue0 + "1)");
+    document.documentElement.style.setProperty("--randomColor1", randomValue1 + "1)");
+}
+
+setInterval(settingNewValue, 1000);
